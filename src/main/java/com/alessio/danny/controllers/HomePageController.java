1efd66d638f96e960bc7cf9a360467db7ace5ce3
SL\alessioprofeta <alessio.profeta@osudio.com>
2018-01-02 10:36:14 +0100

    First commit

---
+++ src/main/java/com/alessio/danny/controllers/HomePageController.java
@@ -0,0 +1,15 @@
+package com.alessio.danny.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomePageController {
+
+	@RequestMapping(value = "/")
+    public String showPage(Model model) {
+        model.addAttribute("greetings", "Welcome to the Osudio Blog!");
+        return "pages/homepage";
+    }
+}
